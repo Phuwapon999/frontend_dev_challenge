42d6da8289ab490d170fb60729ad7f0b7073bdae
Phuwapon999 <65011212193@msu.ac.th>
2026-09-10 14:11:48 +0700

    [RES-102] Fix crash on leaving My Orders by cancelling countdown timer on dispose

--- lib/feature/order/widget/pickup_countdown.dart
+++ lib/feature/order/widget/pickup_countdown.dart
@@ -15,12 +15,21 @@ class PickupCountdown extends StatefulWidget {
 }
 
 class _PickupCountdownState extends State<PickupCountdown> {
+  Timer? _timer;
+
   @override
   void initState() {
     super.initState();
-    Timer.periodic(const Duration(seconds: 1), (_) {
+    _timer = Timer.periodic(const Duration(seconds: 1), (_) {
       setState(() {});
     });
+    dispose;
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
   }
 
   @override
